97223d4e954bebe567ae2753de64039b34edc4fa
dipayan2 <dipayan2@illinois.edu>
2024-12-04 10:03:14 -0600

    Our code format works in sequence

--- makeMaude/src/Higher/rewriteSequenceSearch.cc
+++ makeMaude/src/Higher/rewriteSequenceSearch.cc
@@ -115,7 +115,7 @@ RewriteSequenceSearch::findNextInterestingState(){ // this is my playground. I w
     }
   listReturn:
     if (interesting_state_idx < explored_vec.size()){ // We have states ready to be explored
-      printf("[GM] rewriteSequenceSearch::findNextInterestingState() Inside the small loop\n");
+      printf("[GM] rewriteSequenceSearch::findNextInterestingState() Inside the small loop \n");
       int state_id = explored_vec[interesting_state_idx];
       interesting_state_idx++;
       return state_id;
