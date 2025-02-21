7f6a2427f17e871748d9bebca5e3aec7fa3cbc76
dipayan2 <dipayan2@illinois.edu>
2025-02-21 09:26:09 -0600

    Test

--- makeMaude/src/Higher/rewriteSequenceSearch.cc
+++ makeMaude/src/Higher/rewriteSequenceSearch.cc
@@ -89,9 +89,9 @@ RewriteSequenceSearch::findNextMatch()
 					  goal,
 					  MatchSearchState::GC_CONTEXT);
     tryMatch:
-      bool foundMatch = matchState->findNextMatch();
+      bool foundMatch = matchState->findNextMatch();// This is a solution for code
       matchState->transferCountTo(*(getContext()));
-      if (foundMatch)
+      if (foundMatch) 
 	return true;
       delete matchState;
     }
@@ -101,7 +101,7 @@ RewriteSequenceSearch::findNextMatch()
 }
 
 int
-RewriteSequenceSearch::findNextInterestingState(){ // this is my playground. I will use this function to play around, and ensure it doesn't break things
+RewriteSequenceSearch::findNextInterestingStateP(){ // this is my playground. I will use this function to play around, and ensure it doesn't break things
 
   printf("[GM] rewriteSequenceSearch::findNextInterestingState()\n");
   if (needToTryInitialState)
@@ -278,7 +278,7 @@ RewriteSequenceSearch::findNextInterestingState(){ // this is my playground. I w
 }
 
 int
-RewriteSequenceSearch::findNextInterestingStateP() // this is the original code
+RewriteSequenceSearch::findNextInterestingState() // this is the original code
 {
   printf("[GM] rewriteSequenceSearch::findNextInterestingState()\n");
   if (needToTryInitialState)
